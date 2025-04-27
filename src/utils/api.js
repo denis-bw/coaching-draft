b343d036804e3688cc80cd8daedcf0fabf63f745
Denis Belousov <109822229+denis-bw@users.noreply.github.com>
2025-04-27 23:03:07 +0300

    test

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = import.meta.env.VITE_BASE_URL_AUTH;
-const BASE_URL = "https://coaching-draft-backend.onrender.com/api/"
+//const BASE_URL = import.meta.env.VITE_BASE_URL_AUTH;
+ const BASE_URL = "https://coaching-draft-backend.onrender.com/api/"
 axios.defaults.baseURL = BASE_URL
 
 const token = {
